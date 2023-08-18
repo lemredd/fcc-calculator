491fd61e2c9054d5f9ce4105ed1a1f6823a4ed57
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-18 15:06:05 +0800

    chore: make `num_buttons`

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,49 @@ import {
 
 import "./App.css";
 
+const num_buttons = [
+	{
+		"id": "zero",
+		"value": 0,
+	},
+	{
+		"id": "one",
+		"value": 1,
+	},
+	{
+		"id": "two",
+		"value": 2,
+	},
+	{
+		"id": "three",
+		"value": 3,
+	},
+	{
+		"id": "four",
+		"value": 4,
+	},
+	{
+		"id": "5",
+		"value": 5,
+	},
+	{
+		"id": "six",
+		"value": 6,
+	},
+	{
+		"id": "seven",
+		"value": 7,
+	},
+	{
+		"id": "eight",
+		"value": 8,
+	},
+	{
+		"id": "nine",
+		"value": 9,
+	},
+] as const;
+
 function App(): ReactElement {
 
 	return (
