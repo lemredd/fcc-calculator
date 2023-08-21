cc93269928a3587b69752f41fe1ea85035e1b954
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-21 16:59:02 +0800

    feat: add `#clear` and `#decimal` buttons

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,8 @@ function App(): ReactElement {
 			<button id="equals">=</button>
 			{num_buttons.map(({ id, value }) => <button key={id} id={id}>{value}</button>)}
 			{operator_buttons.map(({ id, value }) => <button key={id} id={id}>{value}</button>)}
+			<button id="decimal">.</button>
+			<button id="clear">C</button>
 		</>
 	);
 }
