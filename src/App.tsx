c90f5c5fbe379043e90d8f9b6ed5c7c50fdaaf30
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-30 14:25:56 +0800

    chore: rename condition

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,10 @@ function App(): ReactElement {
 			// Multiplication of negative numbers.
 			// TODO: This could be better...
 			const is_multiplying_to_negative_number = last_character === "*" &&  btn_value === "-";
-			const must_change_last_two_characters = input.endsWith("*-") && btn_value === "+";
+			const must_discard_negative_multiplication = input.endsWith("*-") && btn_value === "+";
 
 			if (!is_last_character_operation || is_multiplying_to_negative_number) set_input(`${input}${btn_value}`);
-			else if (must_change_last_two_characters) set_input(`${input.substring(0,input.length-2)}${btn_value}`);
+			else if (must_discard_negative_multiplication) set_input(`${input.substring(0,input.length-2)}${btn_value}`);
 			else set_input(`${input.substring(0,input.length-1)}${btn_value}`);
 		}
 		else if (is_appending_initially) set_input(`${btn_value}`);
